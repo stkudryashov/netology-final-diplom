d8e94af3e69d7720971ab520d17fd1f283373fc1
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-04-01 14:52:45 +0300

    add password reset

--- django_project/backend/urls.py
+++ django_project/backend/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 
 from backend.views import RegisterAccount, ConfirmAccount
 
+from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 app_name = 'backend'
 
 urlpatterns = [
     path('user/register', RegisterAccount.as_view(), name='user-register'),
-    path('user/register/confirm', ConfirmAccount.as_view(), name='user-register-confirm')
+    path('user/register/confirm', ConfirmAccount.as_view(), name='user-register-confirm'),
+
+    path('user/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('user/password_reset', reset_password_request_token, name='password-reset'),
+    path('user/password_reset/confirm', reset_password_confirm, name='password-reset-confirm'),
 ]
