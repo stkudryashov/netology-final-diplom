2626471675e165e7b6593cc3b3c5e11f23f6a9e7
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-04-06 17:32:37 +0300

    fix docker-compose.yaml

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1%p5i9kc-&b8t!nt!g%gx@lu$o=r6km_po(7f)$_0mm)^ia)li'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
