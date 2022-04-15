832c33705c2021832a5c08103b7e38ff8b21b730
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-04-16 02:21:39 +0300

    add cors config

--- django_backend/config/settings.py
+++ django_backend/config/settings.py
@@ -46,18 +46,23 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'django_rest_passwordreset',
 
+    'corsheaders',
+
     'backend.apps.BackendConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'config.urls'
 
